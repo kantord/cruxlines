1b2bd9ab3cea6f2e328b6ba109b3d954eadd8691
Daniel Kantor <git@daniel-kantor.com>
2025-12-26 21:40:48 +0100

    add petgraph example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    use petgraph::algo::dijkstra;
+    use petgraph::graph::Graph;
+
+    let mut g = Graph::<&str, u32>::new();
+    let a = g.add_node("A");
+    let b = g.add_node("B");
+    g.add_edge(a, b, 1);
+
+    let paths = dijkstra(&g, a, None, |e| *e.weight());
+    println!("{paths:?}");
 }
