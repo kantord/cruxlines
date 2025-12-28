cd4c21b1783cd59b3d54cdf4fd40d0fe1fb801b9
Daniel Kantor <git@daniel-kantor.com>
2025-12-28 21:22:13 +0100

    avoid kebab-case names

--- src/main.rs
+++ src/main.rs
@@ -17,11 +17,11 @@ struct Cli {
 
 #[derive(Copy, Clone, Debug, ValueEnum)]
 enum EcosystemArg {
-    #[value(alias = "py")]
+    #[value(name = "python", alias = "py")]
     Python,
-    #[value(alias = "js", alias = "ts", alias = "tsx")]
+    #[value(name = "javascript", alias = "js", alias = "ts", alias = "tsx")]
     JavaScript,
-    #[value(alias = "rs")]
+    #[value(name = "rust", alias = "rs")]
     Rust,
 }
 
